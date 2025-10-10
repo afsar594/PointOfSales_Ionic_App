498c7b816246b340d09ac5e64b41829d05ed1f51
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-10-10 18:25:42 +0500

    anc

--- src/app/pages/dine-in-table/dine-in-table.component.ts
+++ src/app/pages/dine-in-table/dine-in-table.component.ts
@@ -15,16 +15,31 @@ import { TableService } from 'src/app/services/table.service';
 })
 export class DineInTableComponent implements OnInit {
   tables: string[] = [
-    'T-01', 'T-02', 'T-03', 'T-04', 'T-05', 'T-06',
-    'T-07', 'T-08', 'T-09', 'T-10', 'T-11', 'T-12',
-    'T-13', 'T-14', 'T-15', 'T-16', 'T-17', 'T-18'
+    'T-01',
+    'T-02',
+    'T-03',
+    'T-04',
+    'T-05',
+    'T-06',
+    'T-07',
+    'T-08',
+    'T-09',
+    'T-10',
+    'T-11',
+    'T-12',
+    'T-13',
+    'T-14',
+    'T-15',
+    'T-16',
+    'T-17',
+    'T-18',
   ];
   Alltables: any;
 
-  constructor(private modalController: ModalController,
-    private tableapi: TableService,
-
-  ) { }
+  constructor(
+    private modalController: ModalController,
+    private tableapi: TableService
+  ) {}
 
   closeForm() {
     this.modalController.dismiss();
@@ -35,39 +50,36 @@ export class DineInTableComponent implements OnInit {
       component: OrderTakenComponent,
       cssClass: 'custom-width-modal',
       componentProps: {
-        tableData: table   // ✅ passing the table object
-      }
+        tableData: table, // ✅ passing the table object
+      },
     });
     modal.present();
-
   }
 
   ngOnInit() {
-    this.getAllTable()
+    this.getAllTable();
   }
 
   getAllTable() {
     let payload = {
       areaId: null,
-      areaName: "",
+      areaName: '',
       tableId: null,
-      tableName: "",
-      orderType: "",
-      orderNo: "",
-      orderDateTime: "",
-      waiterName: "",
-      customerName: "",
-      tableStatus: ""
-    }
+      tableName: '',
+      orderType: '',
+      orderNo: '',
+      orderDateTime: '',
+      waiterName: '',
+      customerName: '',
+      tableStatus: '',
+    };
     this.tableapi.getList(payload).subscribe((res: any) => {
-      this.Alltables = res.data.flatMap((area: { areaTables: any[]; }) =>
-        area.areaTables.map(table => ({
+      this.Alltables = res.data.flatMap((area: { areaTables: any[] }) =>
+        area.areaTables.map((table) => ({
           name: table.tableName,
-          Id: table.tableId
+          Id: table.tableId,
         }))
       );
-
-    })
+    });
   }
-
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiURL :"http://103.74.54.207:8078/inspireRest/api/"
-apiURL: 'http://103.74.54.207:8078/inspireRest/api/'
+  apiURL: 'http://194.233.95.37:8092/inspireRest/api/',
 };
